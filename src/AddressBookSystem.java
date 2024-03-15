250615d935a9c67321292ef4b674bcd6f4cc3036
Nisha7781 <nishamali0781@gmail.com>
2024-03-15 13:34:52 +0530

    Edit an esisting contact using its first name.

--- src/AddressBookSystem.java
+++ src/AddressBookSystem.java
@@ -1,5 +1,7 @@
 import java.util.*;
-class Contact {
+
+class Contact
+{
     String firstName;
     String lastName;
     String address;
@@ -9,7 +11,8 @@ class Contact {
     String phoneNumber;
     String email;
 
-    Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+    Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email)
+    {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -19,37 +22,63 @@ class Contact {
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
-    @Override
-    public String toString() {
-        return
-                "firstName= " + firstName + "\n" +
-                        "lastName= " + lastName + "\n" +
-                        "address= " + address + "\n" +
-                        "city= " + city + "\n" +
-                        "state= " + state + "\n" +
-                        "zip= " + zip + "\n" +
-                        "phoneNumber= " + phoneNumber + "\n" +
-                        "email= " + email ;
-    }
 
+    @Override
+    public String toString()
+    {
+        return "firstName= " + firstName + "\n" +
+                "lastName= " + lastName + "\n" +
+                "address= " + address + "\n" +
+                "city= " + city + "\n" +
+                "state= " + state + "\n" +
+                "zip= " + zip + "\n" +
+                "phoneNumber= " + phoneNumber + "\n" +
+                "email= " + email;
+    }
 }
 
-public class AddressBookSystem {
-    private static ArrayList<Contact> contacts = new ArrayList<>();
+public class AddressBookSystem
+{
+    private ArrayList<Contact> contacts = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to Address Book Program.... \n");
 
         AddressBookSystem abs = new AddressBookSystem();
-        abs.addContact();
-        System.out.println("Contacts:");
-        for (Contact contact : contacts) {
-            System.out.println(contact);
+        abs.AddressBook();
+    }
+
+    public void AddressBook()
+    {
+        while (true) {
+            System.out.println("Address Book System");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Edit Contact");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    addContact();
+                    break;
+                case 2:
+                    editContact();
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice...");
+            }
         }
     }
 
-    void addContact() {
+    private void addContact()
+    {
         System.out.print("Enter First Name: ");
         String firstName = sc.nextLine();
         System.out.print("Enter Last Name: ");
@@ -71,5 +100,37 @@ public class AddressBookSystem {
         contacts.add(contact);
         System.out.println();
         System.out.println("Contact added successfully! \n");
+    }
+
+    private void editContact()
+    {
+        System.out.println("Enter the first name of the contact to edit: ");
+        String fnameToEdit = sc.nextLine();
+        for (Contact contact : contacts)
+        {
+            if (contact.firstName.equalsIgnoreCase(fnameToEdit))
+            {
+                System.out.println("Enter the new details:");
+                System.out.print("Enter Last Name: ");
+                contact.lastName = sc.nextLine();
+                System.out.print("Enter Address: ");
+                contact.address = sc.nextLine();
+                System.out.print("Enter City: ");
+                contact.city = sc.nextLine();
+                System.out.print("Enter State: ");
+                contact.state = sc.nextLine();
+                System.out.print("Enter Zip: ");
+                contact.zip = sc.nextLine();
+                System.out.print("Enter Phone Number: ");
+                contact.phoneNumber = sc.nextLine();
+                System.out.print("Enter Email: ");
+                contact.email = sc.nextLine();
+                System.out.println("Contact details updated successfully! \n");
+                System.out.println("Updated Contact Details:");
+                System.out.println(contact);
+                return;
+            }
+        }
+        System.out.println("Contact not found!");
     }
 }
